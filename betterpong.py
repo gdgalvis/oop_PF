7800c83946abf4881f0e85ef044c70d6a3ff16fe
gdgalvis <73660713+gdgalvis@users.noreply.github.com>
2022-09-07 18:16:28 -0500

    Inicio

---
+++ betterpong.py
@@ -0,0 +1,11 @@
+import pygame 
+pygame.init()
+Ancho =700
+Alto = 500
+Pantalla = pygame.display.set_mode((Ancho, Alto))
+pygame.display.set_caption("Proyecto Final")
+
+#Parametros
+fps = 60
+blanco = (255, 255, 255)
+negro = (0, 0, 0)
