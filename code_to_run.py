a56cc1a42237808da278639b7f0b18d702ae81cd
Dtornet <110875651+Dtornet@users.noreply.github.com>
2022-11-16 21:46:47 -0500

    Update code_to_run.py

--- code_to_run.py
+++ code_to_run.py
@@ -1,3 +1,11 @@
+#This file is part of betterpong.
+
+#Betterpong is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+#Betterpong is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License along with Betterpong. If not, see <https://www.gnu.org/licenses/>.
+#«Copyright 2022 Galvis German, Tornet Diego»
 
 from betterpong import pygame, Paleta,Bola,Jugar
 from pygame.locals import *
